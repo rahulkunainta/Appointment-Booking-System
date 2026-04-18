62e70b457710917621f25918e4329e7008ba15ed
Rahul Kunainta <rahulkunainta1@gmail.com>
2026-04-18 23:00:37 +0530

    Change database password in DBConnection
    Updated the database password in the DBConnection class.

--- src/main/java/com/project/util/DBConnection.java
+++ src/main/java/com/project/util/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
 		
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/doctor_db","mysql.railway.internal","NewPassword123");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/doctor_db","mysql.railway.internal","OAVwGuYkahxNJFIYrlBxTKYEHSsKUbOD");
 		} catch (Exception e) {
 			
 			e.printStackTrace();
