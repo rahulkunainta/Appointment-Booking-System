2f9b0fd25e0cf9e2a0c5cc5cba05fe00f2c46f51
Rahul Kunainta <rahulkunainta1@gmail.com>
2026-04-19 00:19:02 +0530

    Refactor DBConnection for dynamic MySQL connection
    Refactored DBConnection to use environment variables for connection parameters.

--- src/main/java/com/project/util/DBConnection.java
+++ src/main/java/com/project/util/DBConnection.java
@@ -3,24 +3,28 @@ package com.project.util;
 import java.sql.*;
 
 public class DBConnection {
-	
-	public static Connection getConnection () {
-		Connection con = null;
-		
-		try{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(
-    "jdbc:mysql://roundhouse.proxy.rlwy.net:53520/doctor_db",
-    System.getenv("root"),
-    System.getenv("OAVwGuYkahxNJFIYrlBxTKYEHSsKUbOD")
-);
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-			
-		}
-		return con;
-		
-	}
 
+    public static Connection getConnection() {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://" 
+                       + System.getenv("MYSQLHOST") + ":" 
+                       + System.getenv("MYSQLPORT") + "/" 
+                       + System.getenv("MYSQLDATABASE")
+                       + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+            String user = System.getenv("MYSQLUSER");
+            String pass = System.getenv("MYSQLPASSWORD");
+
+            con = DriverManager.getConnection(url, user, pass);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
 }
